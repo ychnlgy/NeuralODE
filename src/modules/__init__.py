8d416aed5c63586eb828fe19732680c7c1ba831f
Yuchen Li <ychnlgy@gmail.com>
2019-01-22 12:31:36 -0500

    Removed blueprinter

--- src/modules/__init__.py
+++ src/modules/__init__.py
@@ -7,8 +7,6 @@ from .Upsample import Upsample
 from .Reshape import Reshape
 from .Operation import Operation
 
-from .Blueprinter import Blueprinter
-
 from .ResBlock import ResBlock
 from .RexBlock import RexBlock
 from .ResNet import ResNet
