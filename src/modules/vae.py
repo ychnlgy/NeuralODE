957800f4cff9fd8057a795f032654f1ad687e4f6
Yuchen Li <ychnlgy@gmail.com>
2019-01-23 18:34:29 -0500

    Debugging

--- src/modules/vae.py
+++ src/modules/vae.py
@@ -126,4 +126,4 @@ class VAE(torch.nn.Module):
         return self.decoder(self.encoder(X), t)
     
     def loss(self, lossf, X, Xh):
-        return lossf(Xh, X) + 0.01 * self.encoder.loss()
+        return lossf(Xh, X) + 0.00 * self.encoder.loss()
